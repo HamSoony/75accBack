0c2d9c03d37c2f8ec4a5c33f8c33e9acada1d7bd
HamSoony <131135854+HamSoony@users.noreply.github.com>
2023-06-07 15:31:59 +0900

    Update AccountingApplication.java

--- src/main/java/com/project/Accounting/AccountingApplication.java
+++ src/main/java/com/project/Accounting/AccountingApplication.java
@@ -2,10 +2,8 @@ package com.project.Accounting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
-@RefreshScope
 public class AccountingApplication {
 
 	public static void main(String[] args) {
