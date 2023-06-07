319a4a3cdde067b2f9f7999afcc1071070e4aba6
CodaeSeong <sdaeseong.kim@gmail.com>
2023-06-07 15:28:44 +0900

    config

--- src/main/java/com/project/Accounting/AccountingApplication.java
+++ src/main/java/com/project/Accounting/AccountingApplication.java
@@ -2,10 +2,10 @@ package com.project.Accounting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
+
 
 @SpringBootApplication
-@RefreshScope
+
 public class AccountingApplication {
 
 	public static void main(String[] args) {
